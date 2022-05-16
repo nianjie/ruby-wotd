afbd30337cc29c739e6a15b07b52eaf9e83965f7
nj <4769034+nianjie@users.noreply.github.com>
2022-05-16 15:20:02 +0900

    dict called twice if access other routes than /today.

--- dic.rb
+++ dic.rb
@@ -1,5 +1,6 @@
 class Dictionary
   def self.wordOfTheDay(the_day = Date.today)
+    puts the_day
     word = {title: 'audit', definition: 'audit', link: 'https://www.oxfordlearnersdictionaries.com/definition/english/audit_1', updated: '2020-12-17T01:00:00Z'}
   end
 end
