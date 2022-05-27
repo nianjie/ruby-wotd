7c114a6191024d9dccfc0c479b856f27850a8630
nj <4769034+nianjie@users.noreply.github.com>
2022-05-27 16:12:42 +0900

    config with settings from environment.

--- firebase/client.rb
+++ firebase/client.rb
@@ -1,9 +1,15 @@
 require 'faraday'
+require 'json'
 
 class Client
+  # The name of the environment variable to lookup a Firebase config.
+  FIREBASE_CONFIG_ENV_VAR = "FIREBASE_CONFIG"
   USER_AGENT = "Ruby/#{RUBY_VERSION}"
+
   def initialize(**options)
-    @root = options[:databaseURL]
+    raise %Q[the environment `#{FIREBASE_CONFIG_ENV_VAR}' was not set.] unless ENV[FIREBASE_CONFIG_ENV_VAR]
+    config = JSON.parse(ENV[FIREBASE_CONFIG_ENV_VAR])
+    @root = config['databaseURL']
   end
   
   def get(location)
