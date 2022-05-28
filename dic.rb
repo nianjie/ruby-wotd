338360a598074add521009af19160dec38e28c8f
nj <4769034+nianjie@users.noreply.github.com>
2022-05-28 14:15:01 +0900

    move configuration into environment. (#2)
    * config with settings from environment.

--- dic.rb
+++ dic.rb
@@ -1,20 +1,19 @@
 require './firebase/client.rb'
 
 class Dictionary
-  FIREBASE_ROOT = %q[https://q8732.firebaseio.com/]
-  class_eval do |_|
-    @db_client = Client.new(databaseURL: FIREBASE_ROOT)
-  end
-
-  def self.wordOfTheDay(the_day = Date.today)
-    res = @db_client.get("/chronological/#{the_day.strftime('%Y/%-m/%-d')}")
-    word = getWord(res.body) if res.body
-  end
-  
   class << self
+    def wordOfTheDay(the_day = Date.today)
+      res = db_client.get("/chronological/#{the_day.strftime('%Y/%-m/%-d')}")
+      word = getWord(res.body) if res.body
+    end
+  
     def getWord(word)
-      res = @db_client.get "/word/#{word}"
+      res = db_client.get "/word/#{word}"
       res.body
+    end
+
+    def db_client
+      @db_client ||= Client.new
     end
   end
 
